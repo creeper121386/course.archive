9994dfa5d9240d570177d2940beaa61985693317
creeper121386 <694029828@qq.com>
2020-05-25 17:27:36 +0800

    finish exp1

---
+++ exp1/sample.c
@@ -0,0 +1,65 @@
+// line comment
+/* block comment */
+
+/* 
+block comment over multiple lines
+*/
+
+int 1a, b, c; // line comment after code
+int d = 1;
+float e, f;
+
+
+
+char g = 'c', h;
+int array[10];
+string s;
+
+struct MyStruct
+{
+    int member1;
+    string member2;
+} mystruct;
+
+int function(int param1, int param2)
+{
+    printf("I am a lovely function~ qwq");
+    return param1 + param2;
+}
+
+int main()
+{
+    int anotherArray = [ 2, 3, 3 ];
+    int array2d[2][2] = [ [ 1, 2 ], [ 3, 4 ] ];
+    int i = 0;
+    // declaration at the beginning of code block.
+
+    string s = "wow! you can assign struct directly!";
+    s = "now I have new value.";
+
+    anotherArray[2] = 233;
+    mystruct.member1 = 0;
+    mystruct.member2 = "ohhhhhhhhh!!!!!!";
+
+    while (1)
+    {
+        i++;
+        i += 1;
+        i *= 1;
+        i /= 1;
+        i %= 1;
+
+        if (i == 666)
+            break;
+        else if (i == 233)
+            continue;
+        else
+            printf("running...");
+    }
+
+    for (i = 0; i < 233; i++)
+    {
+        // do nothing
+    }
+    return 1;
+}
--- run.sh
+++
@@ -1,5 +0,0 @@
-rm parser.tab.c parser.tab.h parser parser.output lex.yy.c
-flex lex.l
-bison -vd parser.y
-gcc -g -o parser lex.yy.c parser.tab.c ast.c
-# ./parser test.c
